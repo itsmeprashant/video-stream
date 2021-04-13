4958aa3dd5ef9f679605742f366a037b9181b2e0
Prashant Sharma <itsmeprashant58@gmail.com>
2021-04-14 01:18:12 +0530

    change port

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ app.get("/video", function (req, res) {
   videoStream.pipe(res);
 });
 
-app.listen(8443, function () {
+app.listen(process.env.PORT || 8000, function () {
   console.log("Listening on port 8000!");
 });
